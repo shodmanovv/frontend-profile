e9f4182bfe3618b4f7ea4cf03a170aaf4ad0758c
Pulat Shodmanov <pshodmanovv@gmail.com>
2024-06-28 15:10:14 +0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import axios from 'axios'
+import router from './router'
+
+axios.defaults.baseURL = "https://backend-profile.vercel.app/api/v1"
+
+// axios.defaults.baseURL = "http://127.0.0.1:8000/api/v1/post/"
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router, axios)
+
+app.mount('#app')
